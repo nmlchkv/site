4c4abd4894edf487a35f16484bf070837a20d882
nmlchkv <48715986+nmlchkv@users.noreply.github.com>
2024-11-26 01:08:02 +0300

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,1 +1,7 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('404 Page Loaded');
+    const title = document.querySelector('.error-title');
+    title.style.transition = 'transform 1s ease-in-out';
+    title.style.transform = 'rotate(360deg)';
+});

d69903e26396dbbdb39c03c1d2f0fecbe5ef5639
nmlchkv <48715986+nmlchkv@users.noreply.github.com>
2024-11-26 10:52:18 +0300

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const text = document.getElementById('falling-text');
   const letters = text.querySelectorAll('span');
-
   letters.forEach(letter => {
     const randomDelay = Math.random() * 2 + 's';  // Random delay between 0 and 2 seconds
     letter.style.animationDelay = randomDelay;

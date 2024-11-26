2a89a7628c89e217d6cbb98619c3af60a5e1b113
nmlchkv <48715986+nmlchkv@users.noreply.github.com>
2024-11-26 10:39:23 +0300

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const text = document.getElementById('falling-text');
   const letters = text.querySelectorAll('span');
 
-  // Set a random animation delay for each letter so they start falling at different times
   letters.forEach(letter => {
     const randomDelay = Math.random() * 2 + 's';  // Random delay between 0 and 2 seconds
     letter.style.animationDelay = randomDelay;
